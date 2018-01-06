c119ef9d2df8b7165c21856d0fb9f449466cd20b
iamsimranjot <iamsimranjot@gmail.com>
2018-01-06 23:12:10 +0530

    Configured Game Play

--- Guess the Flag/Guess the Flag/ViewController.swift
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
     
@@ -16,11 +17,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
     
     // MARK: LifeCycle Mathods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setAppearanceFor(button: button1)
+        setAppearanceFor(button: button2)
+        setAppearanceFor(button: button3)
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+        
+    }
+    
+    func setCountriesButtons() {
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    func setAppearanceFor(button: UIButton) {
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as? [String] ?? [String]()
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        
+        title = countries[correctAnswer].uppercased()
+        
+        setCountriesButtons()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
         
     }
 }
