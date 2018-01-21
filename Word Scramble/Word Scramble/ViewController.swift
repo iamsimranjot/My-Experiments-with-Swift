857eb4d3d13af5e9f630f86d750b9aecefb1773a
iamsimranjot <iamsimranjot@gmail.com>
2018-01-21 18:06:28 +0530

    Core Logic Implemented

--- Word Scramble/Word Scramble/ViewController.swift
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         loadWordsFromResource()
         startGame()
     }
@@ -45,6 +47,59 @@ class ViewController: UITableViewController {
                 allWords = ["silkworm"]
             }
         }
+    }
+    
+    @objc func promptForAnswer() {
+        
+        let alertController = UIAlertController.init(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let submitAction = UIAlertAction.init(title: "Submit", style: .default) { [unowned self, alertController] _ in
+            
+            let answer = alertController.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        
+        alertController.addAction(submitAction)
+        present(alertController, animated: true)
+    }
+    
+    func submit(answer: String) {
+        
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) && isOriginal(word: lowerAnswer) && isReal(word: lowerAnswer) {
+            
+            usedWords.insert(answer, at: 0)
+            
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return true
+    }
+}
+
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 }
 
