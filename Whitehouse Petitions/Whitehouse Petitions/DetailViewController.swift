14f0df3ea30fa2f5c718445520a22171a357ab9e
iamsimranjot <iamsimranjot@gmail.com>
2018-01-25 23:53:41 +0530

    Add & Configure DetailViewController

--- Whitehouse Petitions/Whitehouse Petitions/DetailViewController.swift
+++ Whitehouse Petitions/Whitehouse Petitions/DetailViewController.swift
@@ -7,29 +7,39 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadHTML()
     }
-    */
-
+    
+    func loadHTML() {
+        
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+    }
 }
