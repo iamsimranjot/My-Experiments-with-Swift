fa9d5824547820ae279bc8c53f56ec77a29a3ab0
iamsimranjot <iamsimranjot@gmail.com>
2018-01-25 23:53:06 +0530

    Add SwiftJSON and Configure View Controller

--- Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var petitions = [[String: String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        getPetitions()
     }
     
+    func getPetitions() {
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = URL(string: urlString) {
+            
+            if let data = try? String(contentsOf: url) {
+                let json = JSON(parseJSON: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    parse(json: json)
+                }
+            }
+        }
+    }
     
+    func parse(json: JSON) {
+        
+        for result in json["results"].arrayValue {
+            
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            
+            petitions.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        
+        return cell
+    }
 }
 
