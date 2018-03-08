12abbfeb7685581981b1b5d4b9d3a4f59a81df0d
iamsimranjot <iamsimranjot@gmail.com>
2018-03-08 16:11:00 +0530

    - Added Level 1 Resource File - Configured User Interface with Level Specifics

--- Swifty Words/Swifty Words/View Controllers/ViewController.swift
+++ Swifty Words/Swifty Words/View Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
     
@@ -19,11 +20,78 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     
     
+    // MARK: Properties
+    
+    var solutions = [String]()
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    
+    var score = 0
+    var level = 1
+    
     // MARK: Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        prepareLetterButtons()
+        loadLevel()
+    }
+    
+    
+    // MARK: Helper Functions
+    
+    func prepareLetterButtons() {
+        
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+    }
+    
+    func loadLevel() {
+        
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let resourcePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            
+            if let levelContents = try? String(contentsOfFile: resourcePath) {
+                
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                
+                for (index, line) in lines.enumerated() {
+                    
+                    let parts = line.components(separatedBy: ":")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString = "\(index + 1)." + "\(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if letterBits.count == letterButtons.count {
+            for i in 0 ..< letterBits.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
+    
+    @objc func letterTapped(btn: UIButton) {
     }
     
     
