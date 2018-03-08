3efd40990005d3af01af3e9c232b5bc65bdbcf35
iamsimranjot <iamsimranjot@gmail.com>
2018-03-08 15:29:55 +0530

    - Laid out UI Interface along with constraints - Connected required Outlets & Actions

--- Swifty Words/Swifty Words/View Controllers/ViewController.swift
+++ Swifty Words/Swifty Words/View Controllers/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // MARK: IBOutlets
 
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    // MARK: Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    // MARK: IBActions
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func submitTapped(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
 }
 
